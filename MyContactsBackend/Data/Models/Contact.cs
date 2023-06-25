50d15b006cddaaa584ea7c308b3265c8dac5a585
gregory cozer <gregory.cozer@gmail.com>
2023-06-25 18:08:02 -0300

    configuração pronta do EF com migrações

---
+++ MyContactsBackend/Data/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using Data.Repositories;
+using System.Buffers.Text;
+
+namespace Data.Models
+{
+    public class Contact : BaseModel
+
+    {
+       
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+
+    }
+}
